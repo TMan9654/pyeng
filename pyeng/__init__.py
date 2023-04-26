3993b86cbdcfc2d30f2d8c91bfbc325bcdd61ed8
TMan9654 <terrinjburns@gmail.com>
2023-04-26 00:52:44 -0600

    Initial file upload
    Initial upload of UNDER DEVELOPMENT files

---
+++ pyeng/__init__.py
@@ -0,0 +1,11 @@
+from .utils import *
+from .mechanical import *
+unit_system = "IMPERIAL [IN]"
+u_length = "in"
+u_area = "in²"
+u_volume = "in³"
+u_inertia = "in⁴"
+u_mass = "lb"
+u_pressure = "psi"
+u_pload = "lbf"
+u_dload = "lbf/in"
